32553ff5f259539e3ecc77f4687dcfb4f9f3190f
opipi <opipi406@gmail.com>
2022-01-03 21:54:37 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# クリスタWebデザインカンプ 上級編
+
+PENGIN BLOG様（[https://crestadesign.org](https://crestadesign.org)）提供のデザインカンプの実装
+
+### 参考ページ
+[【コーディング課題】XDからコーディングをしよう【上級編】](https://crestadesign.org/cording-third/)
+
+# 使用したフレームワーク等
+- Sass（SCSS）
+- destyle.css
+- jQuery3.6.0
---
+++ js/script.js
@@ -0,0 +1,34 @@
+$(function () {
+    const
+        $window = $(window),
+        $logo = $(".header__logo"),
+        $title = $(".header__title"),
+        $burgerMenu = $(".burger-menu");
+    const boundaryTitleY = 620, boundaryMenuY = 700;
+    const jsClsName = "js-reverce-color";
+
+    // ヘッダー内文字色の反転処理
+    $(window).on("load scroll", function () {
+        // キービジュアルの高さを超えたらロゴとタイトルの色を反転
+        if ($logo.hasClass(jsClsName) && $window.scrollTop() <= boundaryTitleY) {
+            $logo.removeClass(jsClsName);
+            $title.removeClass(jsClsName);
+        }
+        if (!$logo.hasClass(jsClsName) && $window.scrollTop() > boundaryTitleY) {
+            $logo.addClass(jsClsName);
+            $title.addClass(jsClsName);
+        }
+        // キービジュアルの黒背景の高さを超えたらハンバーガーメニューの色を反転
+        if ($burgerMenu.hasClass(jsClsName) && $window.scrollTop() <= boundaryMenuY) {
+            $burgerMenu.removeClass(jsClsName);
+        }
+        if (!$burgerMenu.hasClass(jsClsName) && $window.scrollTop() > boundaryMenuY) {
+            $burgerMenu.addClass(jsClsName);
+        }
+    });
+
+    // ハンバーガーボタンのクリックイベント
+    $("body").on("click", ".burger-menu", function () {
+        $(".burger-menu, .burger-menu__lines, #overlay").toggleClass("open");
+    });
+});
